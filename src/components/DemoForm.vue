72cbcee823d7844361d233538e3ba9febadfbd9b
DESKTOP-RTMI3J7\XuanTruong <xuantruong28x@gmail.com>
2024-10-28 18:38:35 +0700

    Upload Vue-Form

---
+++ src/components/DemoForm.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="container">
+        <h1>Demo Form</h1>
+        <form @submit.prevent="handleSubmit">
+            <div class="form-group">
+                <label for="taikhoan">Tai Khoan</label>
+                <input type="text" 
+                class="form-control" 
+                id="taiKhoan" 
+                v-model="taiKhoan"
+                />
+            </div>
+            <button type="submit" class="btn btn-success">
+                Submit
+            </button>
+        </form>
+    </section>
+</template>
+
+<script>
+export default ({
+    data() {
+        return {
+            taiKhoan: ""
+        };
+    },
+    methods : {
+        handleSubmit(){
+            console.log(this.taiKhoan);
+        },
+    }
+})
+</script>
+
+<style>
+
+</style>
